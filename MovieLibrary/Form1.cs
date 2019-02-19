cdbf72c358282805b1edd93c8381c0fa8330bccd
Edward <Edward@DESKTOP-V62I65P>
2019-02-19 18:19:14 -0500

    Added insert feature

--- MovieLibrary/Form1.cs
+++ MovieLibrary/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 // This is the code for your desktop app.
 // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
@@ -29,7 +31,46 @@ namespace MovieLibrary
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Thanks!");
+            if (movieTextBox.TextLength > 0) { 
+                try
+                {
+                string connection = "datasource=localhost;port=3306;username=movie;password=password";
+                
+                using (MySqlConnection conn = new MySqlConnection(connection))
+                {
+                    MySqlCommand command = new MySqlCommand("INSERT into library.movies(name, year) values('" + movieTextBox.Text + "', 2006)", conn);
+                    MySqlCommand check = new MySqlCommand("Select * from library.movies where name='" + movieTextBox.Text + "'", conn);
+                    MySqlDataReader MyReader2;
+                    
+                        conn.Open();
+                        MyReader2 = check.ExecuteReader();
+                        if (!MyReader2.HasRows)
+                        {
+                        MyReader2.Close();
+                            MyReader2 = command.ExecuteReader();
+                            while (MyReader2.Read())
+                            {
+
+                            }
+                            MessageBox.Show("Added to table");
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Already Exists");
+                        }
+                        conn.Close();
+                    }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            } else
+            {
+                MessageBox.Show("Field Cannot Be Empty!");
+            }
         }
     }
 }
